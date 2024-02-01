0122de402005cb37efe71a995040b9f56e3b7140
Jay Rabjohns <52382074+jayrabjohns@users.noreply.github.com>
2024-02-01 12:24:57 +0000

    feat: placeholder module and data type

---
+++ app/SPCF.hs
@@ -0,0 +1,29 @@
+module SPCF (Term (..), Type (..)) where
+
+type Var = String
+
+data Term
+  = Variable Var
+  | Lambda Var Type Term
+  | Apply Term Term
+
+instance Show Term where
+  show = beautify 0
+    where
+      beautify :: Int -> Term -> String
+      beautify _ (Variable x) = x
+      beautify i (Lambda var t term) = if i /= 0 then "(" ++ s ++ ")" else s where s = "\\" ++ var ++ ": " ++ show t ++ " . " ++ beautify 0 term
+      beautify i (Apply lhs rhs) = if i == 2 then "(" ++ s ++ ")" else s where s = beautify 1 lhs ++ " " ++ beautify 2 rhs
+
+data Type
+  = Base
+  | (:->) Type Type
+  deriving (Eq)
+
+instance Show Type where
+  show = beautify
+    where
+      beautify :: Type -> String
+      beautify Base = "o"
+      beautify (Base :-> rhs) = "o -> " ++ beautify rhs
+      beautify (lhs :-> rhs) = "(" ++ beautify lhs ++ ") -> " ++ beautify rhs
