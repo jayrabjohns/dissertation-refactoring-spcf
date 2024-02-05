32e81bca2be4c82b318a6688a4818e101069141e
Jay Rabjohns <52382074+jayrabjohns@users.noreply.github.com>
2024-02-05 22:23:31 +0000

    attempt at monadic implementation of example

--- test/MonadicExampleSpec.hs
+++ test/MonadicExampleSpec.hs
@@ -1,7 +1,7 @@
 module MonadicExampleSpec where
 
 import qualified Data.Map as Map
-import MonadicExample (Expression (..), Value (..), eval)
+import MonadicExample
 import Test.HUnit
 
 tests :: Test
@@ -13,6 +13,8 @@ reduceExpression =
     ( do
         -- 12 + 4 + 2 = 18
         let expression = Literal 12 `Plus` Apply (Abstract "x" (Variable "x")) (Literal 4 `Plus` Literal 2)
-        let result = eval Map.empty expression
-        assertEqual "should equal 18" (Nat 18) result
+        let result = runEval (eval Map.empty expression)
+        case result of
+          Right val -> assertEqual "should equal 18" (Nat 18) val
+          Left err -> assertFailure err
     )
