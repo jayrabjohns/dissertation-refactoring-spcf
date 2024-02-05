6c37bdc372c4126c653196142be36fec97629bc2
Jay Rabjohns <52382074+jayrabjohns@users.noreply.github.com>
2024-02-05 16:57:14 +0000

    wip

--- app/SPCF.hs
+++ app/SPCF.hs
@@ -11,8 +11,8 @@ instance Show Term where
   show = beautify 0
     where
       beautify :: Int -> Term -> String
-      beautify _ (Variable x) = x
-      beautify i (Lambda var t term) = if i /= 0 then "(" ++ s ++ ")" else s where s = "\\" ++ var ++ ": " ++ show t ++ " . " ++ beautify 0 term
+      beautify _ (Variable x) = show x
+      beautify i (Lambda var t term) = if i /= 0 then "(" ++ s ++ ")" else s where s = "\\" ++ show var ++ ": " ++ show t ++ " . " ++ beautify 0 term
       beautify i (Apply lhs rhs) = if i == 2 then "(" ++ s ++ ")" else s where s = beautify 1 lhs ++ " " ++ beautify 2 rhs
 
 data Type
