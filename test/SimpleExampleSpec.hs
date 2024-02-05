033a323479dddea119bd69a17e6b6c08ce7fc4a8
Jay Rabjohns <52382074+jayrabjohns@users.noreply.github.com>
2024-02-05 19:08:28 +0000

    test: sample test suites

---
+++ test/SimpleExampleSpec.hs
@@ -0,0 +1,18 @@
+module SimpleExampleSpec where
+
+import qualified Data.Map as Map
+import SimpleExample (Expression (..), Value (..), eval)
+import Test.HUnit
+
+tests :: Test
+tests = TestList [TestLabel "should reduce an expression" reduceExpression]
+
+reduceExpression :: Test
+reduceExpression =
+  TestCase
+    ( do
+        -- 12 + 4 + 2 = 18
+        let expression = Literal 12 `Plus` Apply (Abstract "x" (Variable "x")) (Literal 4 `Plus` Literal 2)
+        let result = eval Map.empty expression
+        assertEqual "should equal 18" (Nat 18) result
+    )
