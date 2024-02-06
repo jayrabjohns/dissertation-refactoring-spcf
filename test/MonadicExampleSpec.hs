d708c114d0ddbc1c3784105e822db9d70168eca7
Jay Rabjohns <52382074+jayrabjohns@users.noreply.github.com>
2024-02-06 20:11:51 +0000

    finished transformers example

--- test/MonadicExampleSpec.hs
+++ test/MonadicExampleSpec.hs
@@ -13,7 +13,7 @@ reduceExpression =
     ( do
         -- 12 + 4 + 2 = 18
         let expression = Literal 12 `Plus` Apply (Abstract "x" (Variable "x")) (Literal 4 `Plus` Literal 2)
-        let result = runEval (eval Map.empty expression)
+        let result = runEval Map.empty (eval expression)
         case result of
           Right val -> assertEqual "should equal 18" (Nat 18) val
           Left err -> assertFailure err
