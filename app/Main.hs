ab0a42580677ece7f7e7958165b7a0d5e8173036
Jay Rabjohns <52382074+jayrabjohns@users.noreply.github.com>
2024-03-08 12:33:54 +0000

    feat: encapsulate evaluation context in reader monad

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,9 @@ main :: IO ()
 main = do
   leftAdd <- interpretIO (addLeft 5 3)
   rightAdd <- interpretIO (addRight 5 3)
-  _ <- print $ "5 +l 3: " ++ show leftAdd ++ " -- 5 +r 3: " ++ show rightAdd
+  _ <- print $ "5 +l 3 = " ++ show leftAdd
+  _ <- print $ "5 +r 3 = " ++ show rightAdd
   leftAddError <- either fail return (Error Error1 <+ Error Error2)
   rightAddError <- either fail return (Error Error1 +> Error Error2)
-  print $ "(+l) Error1 Error2: " ++ show leftAddError ++ " -- (+r) Error1 Error2: " ++ show rightAddError
+  _ <- print $ "Error1 +l Error2 = " ++ show leftAddError
+  print $ "Error1 +r Error2 = " ++ show rightAddError
